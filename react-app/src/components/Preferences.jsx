89556092235a2d3551014fc23db395307fc0ff33
eramir17 <eramir@uw.edu>
2024-02-20 17:03:21 -0800

    create account

---
+++ react-app/src/components/Preferences.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react"
+export default function Preferences(){
+    [email, setemail ] = useState("")
+    const handlechange =(e)=>{
+        setemail(e.target.value)
+    }
+return(
+    <>
+    <div className="Preferences">
+       <label for ="email_input" >
+
+    email
+
+       </label>
+        <input type = "text" id="email_input" name="email_input" value= {email}onChange={handlechange}/>
+        
+        
+        </div> 
+    
+    </>
+
+)
+}
--- react-app/src/components/Signup.jsx
+++ react-app/src/components/Signup.jsx
@@ -1,20 +1,52 @@
 import { useState } from "react"
 export default function Signup(){
-    [email, setemail ] = useState("")
-    const handlechange =(e)=>{
+   const [email, setemail ] = useState("");
+   const [password, setpassword ] = useState("");
+   const[passfield, setpassfield] = useState("password");
+
+    const handleChange =(e)=>{
         setemail(e.target.value)
+    }
+    const handlePasswordChange =(e)=>{
+        setpassword(e.target.value)
+    }
+    const handleSubmit =(e)=>{
+        setemail("")
+        setpassword("")
+    }
+    const handleShowPassword =(e)=>{
+        if(passfield=="password"){
+            setpassfield("text")
+        }
+        else{
+            setpassfield("password")
+        }
     }
 return(
     <>
-    <div className="Signup">
-       <label for ="email_input" >
+    <div className="signup">
+        <h1>
+            create an account
+        </h1>
+       <label htmlFor ="email_input" >
 
     email
 
        </label>
-        <input type = "text" id="email_input" name="email_input" value= {email}onChange={handlechange}/>
+        <input type = "text" id="email_input" name="email_input" value= {email} onChange={handleChange}/>
+        <br />
         
-        
+        <label htmlFor ="password_input" >
+
+password
+
+   </label>
+    <input type = {passfield} id="password_input" name="password_input" value= {password} onChange={handlePasswordChange}/> 
+    <br/>
+    <input type="submit" value="signup" onClick={handleSubmit}/>  
+    <button onClick={handleShowPassword}>
+        show password
+    </button>
         </div> 
     
     </>
