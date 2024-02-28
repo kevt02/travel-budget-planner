c4dd788265be63c3440d84ac2761ecc08834ac82
Khin_Win <kwin2@uw.edu>
2024-02-28 15:53:42 -0800

    commit to main

---
+++ frontend/src/App.js
@@ -0,0 +1,40 @@
+
+import React from 'react'
+import Header from './components/Header'
+
+import "./App.css";
+import Sidebar from './components/Sidebar';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './routes/Home';
+import Savings from './routes/Savings';
+import Flights from './routes/Flights';
+import Trains from './routes/Trains';
+import Stays from './routes/Stays';
+import CreateAccount from './routes/CreateAccount';
+import CreateAccountDetail from './routes/CreateAccountDetail';
+import LogIn from "./components/LogIn"; 
+
+function App() {
+    return (
+        <>
+            <BrowserRouter>
+            {/* <Header /> */}
+            <Sidebar />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/savings" element={<Savings />} />
+                    <Route path="/flights" element={<Flights />} />
+                    <Route path="/trains" element={<Trains />} />
+                    <Route path="/stays" element={<Stays />} />
+                    <Route path="/createaccount" element={<CreateAccount />} />
+                    <Route path="/createaccountdetail" element={<CreateAccountDetail />} />
+                    <Route path = "/login" element = {<LogIn />} />
+                </Routes>
+            </BrowserRouter>
+
+        </>
+
+    )
+}
+
+export default App
---
+++ frontend/src/routes/Savings.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Savings() {
+  return (
+    <div className="savings"><h1>Savings</h1></div>
+  )
+}
+
+export default Savings
