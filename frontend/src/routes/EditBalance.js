112c77b4519545168e96184102b1b4de99589145
Kevin Truong <kht5832@gmail.com>
2024-03-10 11:20:43 -0700

    pull this

--- frontend/src/routes/EditBalance.js
+++ frontend/src/routes/EditBalance.js
@@ -1,14 +1,12 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import { useAuth } from '../components/AuthContext';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
 function EditBalance() {
-
-    const [balance, setBalance] = useState("");
+    const [balance, setBalance] = useState(0); 
 
     const { isLoggedIn, uid, login, logout } = useAuth();
-
     const navigate = useNavigate();
 
     useEffect(() => {
@@ -26,18 +24,13 @@ function EditBalance() {
     }, [uid]);
 
     const handleChange = (e) => {
-        setBalance((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+        setBalance(e.target.value); 
     }
-
-
-
-
-
 
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            const response = await axios.put(decodeURI(`http://localhost:2000/${uid}/balance`), balance);
+            const response = await axios.put(`http://localhost:2000/${uid}/balance`, { AccountBalance: balance });
             console.log(response);
             navigate(`/savings`);
         } catch (error) {
@@ -47,19 +40,21 @@ function EditBalance() {
 
     return (
         <div className="edit">
-            <h2>EditBalance</h2>
+            <h2>Edit Balance</h2>
             <form onSubmit={handleSubmit}>
-                <label>Budget</label>
+                <label>Balance</label>
                 <input
                     type="text"
                     name="AccountBalance"
-                    value={balance.AccountBalance}
+                    value={balance}
                     onChange={handleChange}
                 />
-                <button className="accept" type="submit">Accept Changes</button>
+                <button className="accept" type="submit">
+                    Accept Changes
+                </button>
             </form>
         </div>
-    )
+    );
 }
 
-export default EditBalance
+export default EditBalance;
